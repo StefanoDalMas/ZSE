44841f86cd17b8d2118216510f2cb66c33378d55
StefanoDalMas <stefano.dalmas01@gmail.com>
2022-11-27 15:39:49 +0100

    first implementations until get_name() Added file.txt in src to test new_file() wrapper might be a good idea

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,6 @@ fn main() {
     println!("Hello, world!");
     println!("Henlo");
     let mut market = market::ZSE::new_random();
-    let mut wrapper = wrapper::Wrapper::new();
-    // TODO make add do wrapper.markets.push(market);
-    let market2= market::ZSE::new_file("file.txt");
 
 
 }
