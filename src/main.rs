01ea97ffcca97bcaeacbbfa54eb7a76640d391c2
StefanoDalMas <stefano.dalmas01@gmail.com>
2022-11-27 15:38:36 +0100

    first implementations until get_name() Added file.txt in src to test new_file() wrapper might be a good idea

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
+use std::borrow::{Borrow, BorrowMut};
+use unitn_market_2022::market::Market;
+
 mod market;
+mod wrapper;
 
 fn main() {
     println!("Hello, world!");
     println!("Henlo");
-    let market = market::Market::new_default();
-    println!("{:?}", market);
-
+    let mut market = market::ZSE::new_random();
+    let mut wrapper = wrapper::Wrapper::new();
+    // TODO make add do wrapper.markets.push(market);
+    let market2= market::ZSE::new_file("file.txt");
 
 
 }
--- src/market.rs
+++ src/market.rs
@@ -1,25 +1,39 @@
-use unitn_market_2022::good::good::Good;
-use unitn_market_2022::good::consts::STARTING_QTY;
-use unitn_market_2022::good::good_kind::{GoodKind as Gk, GoodKind};
-use unitn_market_2022::market::MarketError;
+use std::borrow::Borrow;
+use std::cell::RefCell;
+use std::fs::File;
+use std::io::BufRead;
+use std::rc::{Rc, Weak};
 use rand::Rng;
+use unitn_market_2022::event::event::Event;
+use unitn_market_2022::event::notifiable::Notifiable;
+use unitn_market_2022::good::good::Good;
+use unitn_market_2022::good::good_kind::{GoodKind as Gk, GoodKind};
+use unitn_market_2022::market::{BuyError, LockBuyError, LockSellError, Market, MarketGetterError, SellError};
+use unitn_market_2022::good::consts::{DEFAULT_EUR_YUAN_EXCHANGE_RATE, DEFAULT_EUR_YEN_EXCHANGE_RATE, DEFAULT_EUR_USD_EXCHANGE_RATE, STARTING_QUANTITY, STARTING_CAPITAL};
+use unitn_market_2022::market::good_label::GoodLabel;
+use crate::wrapper::Wrapper;
 
-
-//TODO LEVARLE
-pub const DEFAULT_EUR_YEN_EXCHANGE_RATE: f32 = 143.615;
-pub const DEFAULT_EUR_USD_EXCHANGE_RATE: f32 = 1.03576;
-pub const DEFAULT_EUR_YUAN_EXCHANGE_RATE: f32 = 7.3599;
-
-#[derive(Debug)]
-pub struct Market{
+pub struct ZSE{
     pub name: String,
     pub goods: [Good; 4],
 }
 
-impl Market{
-    pub(crate) fn new() -> Self{
-        //let mut remaining =  STARTING_QTY;
-        let mut remaining =  1000000.0;
+
+
+
+impl Notifiable for ZSE {
+    fn add_subscriber(&mut self, subscriber: Weak<RefCell<dyn Notifiable>>) {
+        todo!()
+    }
+
+    fn on_event(&mut self, event: Event) {
+        todo!()
+    }
+}
+
+impl Market for ZSE{
+    fn new_random() -> Rc<RefCell<dyn Market>> where Self: Sized {
+        let mut remaining =  STARTING_CAPITAL;
         //create random float number
         let mut tmp = vec![0.0;4];
         let mut rng = rand::thread_rng();
@@ -30,7 +44,7 @@ impl Market{
             remaining -= random_float;
         }
         tmp[3] = remaining;
-        let mut market = Market {
+        let mut market = ZSE {
             name: "ZSE".to_string(),
             goods: [
                 Good::new(Gk::EUR, tmp[0]),
@@ -39,52 +53,97 @@ impl Market{
                 Good::new(Gk::YUAN, tmp[3]*DEFAULT_EUR_YUAN_EXCHANGE_RATE),
             ],
         };
-        market
+        Rc::new(RefCell::new(market))
     }
 
-    pub fn new_default() -> Self{
-        let val = 1000000.0;
-        Market {
+    fn new_with_quantities(eur: f32, yen: f32, usd: f32, yuan: f32) -> Rc<RefCell<dyn Market>> where Self: Sized {
+        let mut market = ZSE {
             name: "ZSE".to_string(),
             goods: [
-                Good::new(Gk::EUR, val),
-                Good::new(Gk::USD, val),
-                Good::new(Gk::YEN, val),
-                Good::new(Gk::YUAN, val),
+                Good::new(Gk::EUR, eur),
+                Good::new(Gk::USD, usd),
+                Good::new(Gk::YEN, yen),
+                Good::new(Gk::YUAN, yuan),
             ],
+        };
+        Rc::new(RefCell::new(market))
+    }
+
+    fn new_file(path: &str) -> Rc<RefCell<dyn Market>> where Self: Sized {
+        let mut file = File::open(path);
+        if file.is_err(){
+            return Self::new_random();
+        }
+        let mut eur = 0.0;
+        let mut usd = 0.0;
+        let mut yen = 0.0;
+        let mut yuan = 0.0;
+        //create BufferedReader
+        let mut reader = std::io::BufReader::new(file.unwrap());
+        for line in reader.lines(){
+            let line = line.unwrap();
+            let mut split = line.split(" ");
+            let good = split.next().unwrap();
+            let quantity = split.next().unwrap();
+            let quantity = quantity.parse::<f32>().unwrap();
+            match good {
+                "EUR" => eur = quantity,
+                "USD" => usd = quantity,
+                "YEN" => yen = quantity,
+                "YUAN" => yuan = quantity,
+                _ => {}
+            }
+        }
+        Self::new_with_quantities(eur, yen, usd, yuan)
+    }
+
+
+    fn get_name(&self) -> &'static str {
+        todo!()
+    }
+
+    fn get_budget(&self) -> f32 {
+        self.goods.iter().map(|good| Self::convert_to_eur(good)).sum()
+    }
+
+    fn get_buy_price(&self, kind: GoodKind, quantity: f32) -> Result<f32, MarketGetterError> {
+        todo!()
+    }
+
+    fn get_sell_price(&self, kind: GoodKind, quantity: f32) -> Result<f32, MarketGetterError> {
+        todo!()
+    }
+
+    fn get_goods(&self) -> Vec<GoodLabel> {
+        todo!()
+    }
+
+    fn lock_buy(&mut self, kind_to_buy: GoodKind, quantity_to_buy: f32, bid: f32, trader_name: String) -> Result<String, LockBuyError> {
+        todo!()
+    }
+
+    fn buy(&mut self, token: String, cash: &mut Good) -> Result<Good, BuyError> {
+        todo!()
+    }
+
+    fn lock_sell(&mut self, kind_to_sell: GoodKind, quantity_to_sell: f32, offer: f32, trader_name: String) -> Result<String, LockSellError> {
+        todo!()
+    }
+
+    fn sell(&mut self, token: String, good: &mut Good) -> Result<Good, SellError> {
+        todo!()
+    }
+}
+
+
+impl ZSE{
+    fn convert_to_eur(g : & Good) -> f32 {
+        match g.get_kind() {
+            Gk::EUR => g.get_qty(),
+            Gk::USD => g.get_qty() / DEFAULT_EUR_USD_EXCHANGE_RATE,
+            Gk::YEN => g.get_qty() / DEFAULT_EUR_YEN_EXCHANGE_RATE,
+            Gk::YUAN => g.get_qty() / DEFAULT_EUR_YUAN_EXCHANGE_RATE,
         }
     }
-
-    pub fn get_market_name(&self) -> &str{
-        &self.name
-    }
-    pub fn get_budget(&mut self) -> Good{
-        self.goods[0].clone()
-    }
-    //TODO
-    pub fn get_buy_price(&self,kind :GoodKind, quantity: i32)-> Result<f32,MarketError> {
-        Err(MarketError::NotImplemented)
-    }
-    pub fn get_sell_price(&self,kind :GoodKind, quantity: i32)-> Result<f32,MarketError> {
-        Err(MarketError::NotImplemented)
-    }
-
-    pub fn get_goods(&self) -> Vec<Good>{
-        self.goods.to_vec()
-    }
-
-    pub fn lock_trader_buy_from_market(&mut self, g: GoodKind, p: f32, q: f32,d: String) -> Result<String,MarketError>{
-        Err(MarketError::NotImplemented)
-    }
-    pub fn trader_buy_from_market(&mut self, token: String, cash: &mut Good) -> Result<Good,MarketError>{
-        Err(MarketError::NotImplemented)
-    }
-    pub fn lock_trader_sell_to_market(&mut self, g: GoodKind, qty: f32, price: f32,d: String) -> Result<String,MarketError>{
-        Err(MarketError::NotImplemented)
-    }
-    pub fn trader_sell_to_market(&mut self, token: String, good: &mut Good) -> Result<Good,MarketError>{
-        Err(MarketError::NotImplemented)
-    }
-
 
 }
