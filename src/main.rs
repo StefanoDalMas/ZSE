1d7f1ee32fada4199b9866455995637399bd544a
marinasegala <marinasegala01@gmail.com>
2022-12-06 11:41:33 +0100

    main change

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ mod wrapper;
 mod tests;
 
 #[derive(Hash)]
-struct Request{
+struct Request_good{
     good_kind: unitn_market_2022::good::good_kind::GoodKind,
     quantity: String,
     offer: String,
@@ -19,9 +19,9 @@ struct Request{
 fn main() {
     println!("Henlo");
     let mut market = market::ZSE::new_random();
-    let x = market.borrow_mut().lock_buy(unitn_market_2022::good::good_kind::GoodKind::USD,5.0,2.0,"prova".to_string());
+    let x = market.borrow_mut().lock_buy(unitn_market_2022::good::good_kind::GoodKind::USD,5.0,2.0,"test".to_string());
     let _ = market.borrow_mut().buy(x.unwrap(), &mut unitn_market_2022::good::good::Good::new(unitn_market_2022::good::good_kind::GoodKind::EUR, 50000.0));
-    let y = market.borrow_mut().lock_sell(unitn_market_2022::good::good_kind::GoodKind::USD,100.0,2.0,"prova".to_string());
+    let y = market.borrow_mut().lock_sell(unitn_market_2022::good::good_kind::GoodKind::USD,100.0,2.0,"test".to_string());
     let _ = market.borrow_mut().sell(y.unwrap(), &mut unitn_market_2022::good::good::Good::new(unitn_market_2022::good::good_kind::GoodKind::USD, 102.0));
 }
 
